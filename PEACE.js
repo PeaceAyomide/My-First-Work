ebb6b2735a0f65d06160360151fa4a1a36f97dfe
PeaceAyomide <133246332+PeaceAyomide@users.noreply.github.com>
2023-05-11 08:46:54 -0700

    Add files via upload

---
+++ PEACE.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLink = document.getElementsByClassName('navbar-link')[0]
+
+
+toggleButton.addEventListener('click', () => {
+    navbarLink.classList.toggle('active')
+})
+
+
